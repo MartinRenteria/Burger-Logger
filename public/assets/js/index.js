9b9340a117a675db4c62b94f6a2908b63f97950b
Martin Renteria <renteriamp96@gmail.com>
2021-01-11 21:44:31 -0800

    Creating names to app features

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,6 +7,14 @@ var burger = {
         });
     },
 
+create: function (name, cb) {
+    orm.create("burgers", [
+        "burger_name", "devoured"
+    ], [
+        name, false
+    ], cb);
+},
+
 };
 
 module.exports = burger;
