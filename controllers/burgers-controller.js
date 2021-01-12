e08068e6cd93ab342f7bc3f12e5d118cc699193d
Martin Renteria <renteriamp96@gmail.com>
2021-01-12 11:58:08 -0800

    initial set up for router to index

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -1,3 +1,10 @@
 let express = require("express");
 let router = express.router();
 let burger = require("../modals/burger.js");
+
+router.get("/", function (req, res) {
+    res.redirect("/burgers");
+});
+
+
+module.exports = router;
