12d1d9d5d380d13935e139585570688d625789ab
Martin Renteria <renteriamp96@gmail.com>
2021-01-12 15:27:01 -0800

    Adding data onto seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- add query
+INSERT INTO burgers SET burger_name = "cheeseburger";
+
+--update query
+UPDATE burgers SET devoured = true WHERE id = 1;
+
+--delete query
+DELETE FROM burgers WHERE id = 1;
