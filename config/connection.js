011e9a9c33c4df44d986b5e3d47db2a0b931c81b
Martin Renteria <renteriamp96@gmail.com>
2021-01-12 15:04:53 -0800

    seeing if this fixes authorization issue with db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
-let mysql = require("mysql");
+require('dotenv').config();
+const mysql = require("mysql");
+const util = require("util");
+let connection;
 
-    connection = mysql.createConnection({
-        host: 'localhost',
+if(process.env.JAWSDB_URL){
+     connection = mysql.createConnection(process.env.JAWSDB_URL)
+     
+}else{
+     connection = mysql.createConnection({
+        host: "localhost",
         port: 3306,
         user: "root",
-        password: "root",
-        database: "anwbeah0duhjuny9"
+        password: process.env.DB_PASS,
+        database: process.env.MYSQL_DB
     });
+}
+
 
 connection.connect(function (err) {
     if (err) {
