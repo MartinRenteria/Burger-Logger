ba02053a3ee176d7cea3d9223284e710d12fdfc6
Martin Renteria <renteriamp96@gmail.com>
2021-01-12 14:57:24 -0800

    updated schema.sql with credentials

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,12 @@
-require("dotenv").config();
 let mysql = require("mysql");
-let connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
     connection = mysql.createConnection({
         host: 'localhost',
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASS,
-        database: process.env.MYSQL_DB
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "anwbeah0duhjuny9"
     });
-}
 
 connection.connect(function (err) {
     if (err) {
