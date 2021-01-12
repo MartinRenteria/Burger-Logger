9f978c6527557d7d88edd73d9e917133acbaef24
Martin Renteria <renteriamp96@gmail.com>
2021-01-11 20:14:51 -0800

    finished setting up server.js file

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 let express = require("express");
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+// Uses files to display infotmation from the public folder to serve static content
+app.use(express.static("public"));
+
+//Parse incoming appiication body data
+app.use(express.uriencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+var handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars);
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Listening on port: ", PORT);
+});
