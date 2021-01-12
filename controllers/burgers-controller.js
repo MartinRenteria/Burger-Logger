7244b05416dcf315e0c8113f509ec3cbdebe72ee
Martin Renteria <renteriamp96@gmail.com>
2021-01-12 14:43:41 -0800

    Finished updating controller js and sending post request

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -14,4 +14,19 @@ router.get("/burgers", function (req, res) {
     });
 });
 
+
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id", function (req, res) {
+    burger.update(req.params.id, function (result) {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
 module.exports = router;
