503d951b0ec1df92595aed063ad7d0feef8176db
Martin Renteria <renteriamp96@gmail.com>
2021-01-12 16:08:12 -0800

    fied typo

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if(process.env.JAWSDB_URL){
         host: "localhost",
         user: "root",
         password: process.env.DB_PASS,
-        database: burger_db
+        database: burgers_db
     });
 }
 
