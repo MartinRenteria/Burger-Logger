8f3267dae802bc521320e4cc6de67591b665cc8e
Martin Renteria <renteriamp96@gmail.com>
2021-01-13 20:01:41 -0800

    Inserted css and defined backup db

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if(process.env.JAWSDB_URL){
         port: 3306,
         user: "root",
         password: process.env.DB_PASS,
-        database: burgers_db
+        database: "burgers_db"
     });
 }
 
